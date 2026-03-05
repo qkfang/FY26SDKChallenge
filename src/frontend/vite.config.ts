6b1d1b5258607370ef1061fd169a4af7f6fa005f
Daniel Fang <qkfang@hotmail.com>
2026-03-05 22:09:53 +1100

    add

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
   return {
+    envPrefix: ['VITE_', 'AZURE_', 'API_URL'],
     plugins: [react()],
     server: {
       port: 3000,
